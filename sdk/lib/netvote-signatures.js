59874b13ac491f873d6ab7698b7c7f438f46854c
Steven Landers <steven.landers@gmail.com>
2018-10-29 22:36:49 -0400

    updating to make sdk browser-friendly

--- sdk/lib/netvote-signatures.js
+++ sdk/lib/netvote-signatures.js
@@ -1,4 +1,4 @@
-const ursa = require("ursa");
+const NodeRSA = require("node-rsa");
 const protobuf = require("protobufjs");
 const uuid = require('uuid/v4');
 
@@ -6,16 +6,38 @@ let Vote;
 
 const initProto = async () => {
     if(!Vote){
-        let root = await protobuf.load(`${__dirname}/vote.proto`);
+        let root = await protobuf.load('./vote.proto');
         Vote = root.lookupType("netvote.Vote");
     }
 }
 
+//for browser compatibility 
+function toBase64( bufferText ) {
+    if(bufferText.indexOf(",") == -1){
+        return bufferText;
+    }
+    let buffer = bufferText.split(","); 
+    var binary = '';
+    var bytes = new Uint8Array( buffer );
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode( bytes[ i ] );
+    }
+    return window.btoa( binary );
+}
+
 const signVote = async (voteBase64) => {
-    let keyPair = ursa.generatePrivateKey();
-    let pub = keyPair.toPublicPem('base64');
-    let data = new Buffer(voteBase64);
-    let sig = keyPair.hashAndSign('md5', data).toString("base64");
+    const key = new NodeRSA();
+    key.setOptions({
+        signingScheme: "pkcs1-md5"
+    })
+    key.generateKeyPair();
+
+    const public = key.exportKey('public');
+    let pub = toBase64(new Buffer(public).toString("base64"));
+
+    let sig = key.sign(new Buffer(voteBase64), null, "base64")
+    
     let obj = {
         signature: sig,
         publicKey: pub
@@ -41,7 +63,7 @@ const encodeVote = async (payload, signatures) => {
 
     let vote = Vote.create(newVote);
     let buff = await Vote.encode(vote).finish();
-    return buff.toString("base64")
+    return toBase64(buff.toString("base64"))
 };
 
 module.exports = {
